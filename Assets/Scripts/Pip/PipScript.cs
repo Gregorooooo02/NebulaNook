bdea116e9fce88ad6f47efdd5eaf716252230bdd
heshsaih <heshsaih@gmail.com>
2025-06-07 23:17:27 +0200

    feat: pip stands up when grabbed and released but its kinda broken

--- Assets/Scripts/Pip/PipScript.cs
+++ Assets/Scripts/Pip/PipScript.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
@@ -6,10 +7,35 @@ public class PipScript : MonoBehaviour
 {
     [SerializeField] public bool IsGrabbed;
     [SerializeField] private XRGrabInteractable pipInteractable;
-    
+    [SerializeField] private GameObject playerObject;
+    private float currentTime = 0;
+    private bool didStandUp = false;
+    [SerializeField] private float timeToStandUp;
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        standUp();
+    }
+
     private void Awake()
     {
         pipInteractable.selectEntered.AddListener((_) => IsGrabbed = true);
         pipInteractable.selectExited.AddListener((_) => IsGrabbed = false);
+    }
+
+    public void TriggerStandUp()
+    {
+        currentTime = 0;
+        didStandUp = false;
+    }
+
+    private void standUp()
+    {
+        if (currentTime >= timeToStandUp && !didStandUp && !IsGrabbed)
+        {
+            gameObject.transform.LookAt(new Vector3(0, 0, 0));
+            didStandUp = true;
+        }
     }
 }
