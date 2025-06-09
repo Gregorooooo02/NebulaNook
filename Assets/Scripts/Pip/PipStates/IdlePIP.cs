dc6e8807bcdc39c228e84d6f8a4072e94e2aa119
Mateusz Podgórski <bazyl0022@gmail.com>
2025-06-09 15:06:37 +0200

    Created basic setup for pip states

---
+++ Assets/Scripts/Pip/PipStates/IdlePIP.cs
@@ -0,0 +1,128 @@
+using UnityEngine;
+
+public class IdlePIP : PipState
+{
+    public bool GotDrink;
+    public DrinkEffect drinkEffect;
+
+    private bool AnimationPlaying = false;
+
+    public float minIdleCooldown;
+    public float maxIdleCooldown; 
+    private float currentCooldownTime;
+    private float currentTime;
+
+    public Animator animator;
+
+    public override PipState RunState()
+    {
+        if(!AnimationPlaying && currentTime >= currentCooldownTime)
+        {
+            PlayRandomIdle();
+        } 
+        else if(currentTime < currentCooldownTime)
+        {
+            currentTime += Time.fixedDeltaTime;
+        }
+
+        if (GotDrink)
+        {
+            switch (drinkEffect)
+            {
+                case DrinkEffect.BLACK_HOLE:
+
+                    break;
+                case DrinkEffect.COMBUTION:
+
+                    break;
+                case DrinkEffect.FREEZE:
+
+                    break;
+                case DrinkEffect.LIFE:
+
+                    break;
+                case DrinkEffect.OIL:
+
+                    break;
+                case DrinkEffect.MATTER:
+
+                    break;
+                case DrinkEffect.EXPLOSION:
+
+                    break;
+                case DrinkEffect.ANIHILATION:
+
+                    break;
+                case DrinkEffect.GRAVITY_LIFT:
+
+                    break;
+                case DrinkEffect.TRANSPARENCY:
+
+                    break;
+                case DrinkEffect.CLONE:
+
+                    break;
+                case DrinkEffect.RAVE:
+
+                    break;
+                case DrinkEffect.POSSESION:
+
+                    break;
+                case DrinkEffect.OVERGROWTH:
+
+                    break;
+                case DrinkEffect.PORTAL:
+
+                    break;
+                case DrinkEffect.NEGATIVE:
+
+                    break;
+                case DrinkEffect.APLAUSE:
+
+                    break;
+                case DrinkEffect.FIREFLIES:
+
+                    break;
+                case DrinkEffect.FROSTFLIES:
+
+                    break;
+                case DrinkEffect.DISSOLVE:
+
+                    break;
+                case DrinkEffect.METEOR:
+
+                    break;
+                case DrinkEffect.TABLE_GLITCH:
+
+                    break;
+                case DrinkEffect.GLITCH:
+
+                    break;
+                case DrinkEffect.WATER:
+                case DrinkEffect.EMPTY:
+                default:
+                    return this;
+            }
+        }
+
+        return this;
+    }
+
+
+    public void PlayRandomIdle()
+    {
+        currentCooldownTime = Random.Range(minIdleCooldown, maxIdleCooldown);
+        //animator.SetBool(); play random idle animation here
+        AnimationPlaying = true;
+    }
+
+
+    public void AnimationEnded()
+    {
+        //Setup it so every time the idle animation ends this method is called
+        AnimationPlaying = false;
+        currentTime = 0;
+    }
+
+
+}
---
+++ Assets/Scripts/Pip/PipStates/PipState.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public abstract class PipState : MonoBehaviour
+{
+    public abstract PipState RunState();
+}
